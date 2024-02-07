f851e9b04f005bcb351a06bd7e053907b96e729b
pele990 <147739451+pele990@users.noreply.github.com>
2024-02-07 22:56:08 +0100

    Delete JavaScript directory

--- JavaScript/datum_i_sat.js
+++
@@ -1,8 +0,0 @@
-function prikaziDatumISat() {
-    const datumISatElement = document.getElementById("datum_i_sat");
-    const datumISat = new Date().toLocaleString();
-    datumISatElement.textContent = datumISat;
-}
-
-prikaziDatumISat();
-setInterval(prikaziDatumISat, 1000);
